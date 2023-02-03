145d503567469437059de10101659a6cbc021f16
John Muskett <john.muskett@pollinate.co.uk>
2023-02-03 21:29:08 +0000

    change

--- src/components/addMessageForm/AddMessageForm.tsx
+++ src/components/addMessageForm/AddMessageForm.tsx
@@ -44,7 +44,7 @@ export const AddMessageForm = () => {
             type="submit"
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           >
-            Click to send
+            send
           </button>
         </div>
       </form>
